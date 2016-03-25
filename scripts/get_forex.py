45f0dab5e06b3bed186b2514eed42d3f18f28639
wvanamstel <wlvanamstel@gmail.com>
2016-03-25 13:34:15 -0700

    able to specify year/month/pair

--- scripts/get_forex.py
+++ scripts/get_forex.py
@@ -3,18 +3,29 @@ import zipfile
 
 
 def download_files():
-    cur_pairs = ["AUDJPY", "AUDNZD", "AUDUSD", "CADJPY", "CHFJPY", "EURCHF", "EURGBP", "EURJPY", "EURUSD",
-                 "GBPJPY", "GBPUSD", "NZDUSD", "USDCAD", "USDCHF", "USDJPY"]
-    years = [str(i) for i in range(2010, 2017)]
-    months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
-    months_verbose = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER",
-                      "OCTOBER", "NOVEMBER", "DECEMBER"]
+    print("Enter currency pair, <enter> for all:")
+    cur_pairs = input()
+    if cur_pairs == "":
+        cur_pairs = ["AUDJPY", "AUDNZD", "AUDUSD", "CADJPY", "CHFJPY", "EURCHF", "EURGBP", "EURJPY", "EURUSD",
+                     "GBPJPY", "GBPUSD", "NZDUSD", "USDCAD", "USDCHF", "USDJPY"]
+    print("Enter year, <enter> for all:")
+    years = input()
+    if years == "":
+        years = [str(i) for i in range(2010, 2017)]
+    print("Enter month, <enter> for all:")
+    months = input()
+    if months == "":
+        months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+
+    months_verbose = {"01": "JANUARY", "02": "FEBRUARY", "03": "MARCH", "04": "APRIL", "05": "MAY", "06": "JUNE",
+                      "07": "JULY", "08": "AUGUST", "09": "SEPTEMBER", "10": "OCTOBER", "11": "NOVEMBER",
+                      "12": "DECEMBER"}
     file_path = "/home/w/data/forex/"
     for year in years:
-        for number, month in enumerate(months):
+        for month in months:
             for pair in cur_pairs:
                 file_name = pair + "-" + year + "-" + month + ".zip"
-                base_url = "http://www.truefx.com/dev/data/" + year + "/" + months_verbose[number] + "-" + year + "/"
+                base_url = "http://www.truefx.com/dev/data/" + year + "/" + months_verbose[month] + "-" + year + "/"
                 print("Downloading {0}".format(file_name))
                 r = requests.get(base_url + file_name)
                 if r.status_code == 200:
