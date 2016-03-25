38e6feb2f0db4cecffcb710fe21a7e84eee9f0be
wvanamstel <wlvanamstel@gmail.com>
2016-03-25 15:51:10 -0700

    bugfix forex model

--- cql/models.py
+++ cql/models.py
@@ -4,13 +4,12 @@ from cassandra.cqlengine import columns
 
 class BaseModel(Model):
     __keyspace__ = "tickdata"
-    __options__ = {"compaction": "LeveledCompactionStrategy",
-                   "sstable_size_in_mb": "64"}
+    __options__ = {"compaction": {"class": "LeveledCompactionStrategy"}}
     __abstract__ = True
 
 
 class Forex(BaseModel):
     forex_pair = columns.Text(primary_key=True)
     tick_time = columns.Time(primary_key=True)
-    bid = columns.Text()
-    ask = columns.Text()
+    bid = columns.Double()
+    ask = columns.Double()
