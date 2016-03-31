33874fe764e547196c84cda79bdf0836f50d50cf
wvanamstel <wlvanamstel@gmail.com>
2016-03-31 10:49:46 -0700

    add constructor

--- scratch.py
+++ scratch.py
@@ -1,6 +1,19 @@
 import requests
+import Quandl
+import pandas as pd
 
 url = "https://www.google.com/finance/getprices?i=60&p=30d&f=d,o,h,l,c,v&df=cpct&q=VXX"
-url = "http://www.netfonds.no/quotes/posdump.php?date=20160303&paper=AA.N&csv_format=csv"
+# url = "http://www.netfonds.no/quotes/posdump.php?date=20160303&paper=AA.N&csv_format=csv"
 
 data = requests.get(url)
+
+vxz=Quandl.get("GOOG/NYSEARCA_VXZ")
+vxx=Quandl.get("GOOG/NYSEARCA_VXX")
+
+pair= pd.DataFrame(index=vxx.index)
+pair["vxx_close"] = vxx["Close"]
+pair["vxz_close"] = vxz["Close"]
+pair=pair.dropna()
+pair["spread"] = pair["vxx_close"]-pair["vxz_close"]
+pair["mean"]=pair["spread"].rolling_mean(window=60)
+pair["std"]=pair["spread"].rolling_std(window=60)
