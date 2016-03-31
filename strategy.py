0493c73778054091e932fc43a1ff52d78dedd6ee
wvanamstel <wlvanamstel@gmail.com>
2016-03-30 18:03:55 -0700

    simple spread strategey

--- strategy.py
+++ strategy.py
@@ -16,12 +16,21 @@ class Strategy(object):
         raise NotImplementedError
 
 
-class Test(object):
+class Test(Strategy):
     def __init__(self, instrument, events_queue, division):
         self.name = instrument
         self.events_queue = events_queue
 
-    def calc_signals(self, event):
+    def calc_signals(self):
         if random.random() > 0.5:
             signal = Signal(self.name, "market", "buy", event.time_stamp)
             self.events_queue.put(signal)
+
+
+class SimpleSpread(Strategy):
+    def __init__(self, instruments, events_queue):
+        self.instr_1 = instruments.iloc[:0]
+        self.instr_2 = instruments.iloc[:1]
+        self.events_queue = events_queue
+
+    def calc_signals(self):
