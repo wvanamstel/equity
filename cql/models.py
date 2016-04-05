6a629de6f74b6814d3ab7a501d06d562465937aa
wvanamstel <wlvanamstel@gmail.com>
2016-04-05 15:41:17 -0700

    change forex model

--- cql/models.py
+++ cql/models.py
@@ -7,10 +7,17 @@ class BaseModel(Model):
     __options__ = {"compaction": {"class": "LeveledCompactionStrategy"}}
     __abstract__ = True
 
+#
+# class Forex(BaseModel):
+#     forex_pair = columns.Text(primary_key=True)
+#     tick_time = columns.DateTime(primary_key=True)
+#     date = columns.Date(partition_key=True)
+#     bid = columns.Double()  # Considered using Decimal, for space considerations use double
+#     ask = columns.Double()
 
 class Forex(BaseModel):
-    forex_pair = columns.Text(primary_key=True)
+    forex_pair = columns.Text(partition_key=True)
+    date = columns.Date(primary_key=True)
     tick_time = columns.DateTime(primary_key=True)
-    date = columns.Date(partition_key=True)
     bid = columns.Double()  # Considered using Decimal, for space considerations use double
     ask = columns.Double()
