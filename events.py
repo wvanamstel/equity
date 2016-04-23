fddb4f2409ad78516b1515142195f5517f7144c3
wvanamstel <wlvanamstel@gmail.com>
2016-04-22 17:33:21 -0700

    use bid/ask event

--- events.py
+++ events.py
@@ -3,12 +3,10 @@ class Event(object):
 
 
 class Tick(Event):
-    def __init__(self, instrument, open, high, low, close, time_stamp):
+    def __init__(self, instrument, bid, ask, time_stamp):
         self.instrument = instrument
-        self.open = open
-        self.high = high
-        self.low = low
-        self.close = close
+        self.bid = bid
+        self.ask = ask
         self.time_stamp = time_stamp
         self.event_type = 'TICK'
 
@@ -16,10 +14,8 @@ class Tick(Event):
         out = 'Time: {}, Event: {}, Instrument: {}, Bid: {}, Ask: {}, Close: {}'.format(str(self.time_stamp),
                                                                                         str(self.event_type),
                                                                                         str(self.instrument),
-                                                                                        str(self.open),
-                                                                                        str(self.high),
-                                                                                        str(self.low),
-                                                                                        str(self.close),
+                                                                                        str(self.bid),
+                                                                                        str(self.ask),
                                                                                         )
         return out
 
@@ -27,25 +23,25 @@ class Tick(Event):
         return str(self)
 
 
-class Signal(Event):
-    def __init__(self, instrument, order, side, time_stamp):
-        self.instrument = instrument
-        self.order = order
-        self.side = side  # buy/sell
-        self.time_stamp = time_stamp
-        self.event_type = 'SIGNAL'
-
-    def __str__(self):
-        out = 'Time: {}, Event: {}, Side: {}, Order: {}, Instrument: {}'.format(str(self.time_stamp),
-                                                                                      str(self.event_type),
-                                                                                      str(self.side),
-                                                                                      str(self.order),
-                                                                                      str(self.instrument),
-                                                                                      )
-        return out
-
-    def __repr__(self):
-        return str(self)
+# class Signal(Event):
+#     def __init__(self, instrument, order, side, time_stamp):
+#         self.instrument = instrument
+#         self.order = order
+#         self.side = side  # buy/sell
+#         self.time_stamp = time_stamp
+#         self.event_type = 'SIGNAL'
+#
+#     def __str__(self):
+#         out = 'Time: {}, Event: {}, Side: {}, Order: {}, Instrument: {}'.format(str(self.time_stamp),
+#                                                                                       str(self.event_type),
+#                                                                                       str(self.side),
+#                                                                                       str(self.order),
+#                                                                                       str(self.instrument),
+#                                                                                       )
+#         return out
+#
+#     def __repr__(self):
+#         return str(self)
 
 
 class Order(Event):
