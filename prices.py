620699fd67888bfa9a65ebd167b0ff42a3f22d83
wvanamstel <wlvanamstel@gmail.com>
2016-04-29 16:46:09 -0700

    add streamer

--- prices.py
+++ prices.py
@@ -2,11 +2,13 @@ import pandas as pd
 import numpy as np
 import Quandl
 
+from decimal import Decimal, getcontext, ROUND_HALF_DOWN
 from datetime import date
 from events import Tick
 from cql.cluster import CqlClient
 from cql.models import *
 
+getcontext().rounding = ROUND_HALF_DOWN
 
 class Prices(object):
     def _price_structure(self):
@@ -69,22 +71,55 @@ class FetchPrices(object):
 
 
 class FetchCassPrices(object):
-    def __init__(self, instruments, model, start_date, end_date=date.today()):
-        self.model = model
-        self.instruments = instruments
-        self.start_date = start_date
-        self.end_date = end_date
-        self.connection = CqlClient(self.model)
+    def __init__(self, events_queue=None):
         self.quotes = dict()
+        self.generator = None
+        self.continue_backtest = True
+        self.events_queue = events_queue
 
-    def get_quotes(self, instruments, model, start_date, end_date=date.today()):
+    def get_quotes(self, instruments, model, start_date, end_date):
+        CqlClient(model)
         for instr in instruments:
             query = model.objects.limit(None).filter(ticker=instr, date__gte=start_date, date__lte=end_date)
             cols = query.first().keys()
             df = pd.DataFrame(columns=cols)
             df = df.append([dict(row) for row in query.all()])
+            df.index = df["time"]
             del df["date"]
             self.quotes[instr] = df
+        self.generator = self.get_generator()
 
-    def get_iterator(self):
-        pass
+    def get_generator(self):
+        return pd.concat(self.quotes.values()).sort_index().iterrows()
+
+    def stream_tick(self):
+        try:
+            index, row = self.generator.__next__()
+        except StopIteration:
+            self.continue_backtest = False
+            return None
+
+        ticker = row["ticker"]
+        close = row["close"]
+        bid = Decimal(str(close)).quantize(
+            Decimal("0.00001")
+        )
+        ask = Decimal(str(close)).quantize(
+            Decimal("0.00001")
+        )
+
+        # Create decimalised prices for traded pair
+        # self.tickers[ticker]["bid"] = bid
+        # self.tickers[ticker]["ask"] = ask
+        # self.tickers[ticker]["timestamp"] = index
+
+        # Create the tick event for the queue
+        tick_event = Tick(ticker, index, bid, ask)
+        self.events_queue.put(tick_event)
+
+
+
+fcp=FetchCassPrices()
+fcp.get_quotes(["wti", "brent"], FuturesMins, "2016-01-05", "2016-01-12")
+it = fcp.get_iterator()
+
